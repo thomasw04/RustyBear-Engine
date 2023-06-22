2333544f0a109b79ad780a0851f6dfcdbb758532
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2023-06-22 21:55:40 +0200

    Renderer concept. Restructuring. More MSAA support.

---
+++ src/buffer.rs
@@ -0,0 +1,75 @@
+use crate::{context::{Context}};
+
+pub struct Framebuffer {
+    texture: wgpu::Texture,
+    sample_count: u32,
+}
+
+impl Framebuffer {
+    pub fn new(context: &Context, sample_count: u32) -> Self
+    {  
+        let texture = context.device.create_texture(&wgpu::TextureDescriptor
+        { 
+            label: Some("Texture"), 
+            size: wgpu::Extent3d {width: context.config.width, height: context.config.height, depth_or_array_layers: 1}, 
+            mip_level_count: 1, 
+            sample_count: sample_count, 
+            dimension: wgpu::TextureDimension::D2, 
+            format: context.config.format, 
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, 
+            view_formats: &context.config.view_formats,
+        });
+
+        Framebuffer { texture: texture, sample_count }
+    } 
+
+    pub fn resize(&mut self, context: &Context, width: u32, height: u32)
+    {
+        if width > 0 && height > 0
+        {
+            let samples = self.texture.sample_count();
+            self.create_buffer(context, samples, width, height);
+        }
+    }
+
+    pub fn change_sample_count(&mut self, context: &Context, sample_count: u32) -> bool
+    {
+        if context.features.texture_features.sample_count_supported(sample_count)
+        {
+            let width = self.texture.width();
+            let height = self.texture.height();
+            self.sample_count = sample_count;
+            self.create_buffer(context, sample_count, width, height);
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn sample_count(&self) -> u32
+    {
+        self.sample_count
+    }
+
+    fn create_buffer(&mut self, context: &Context, sample_count: u32, width: u32, height: u32)
+    {
+        self.texture = context.device.create_texture(&wgpu::TextureDescriptor
+        { 
+            label: Some("Texture"), 
+            size: wgpu::Extent3d {width: width, height: height, depth_or_array_layers: 1}, 
+            mip_level_count: 1, 
+            sample_count: sample_count, 
+            dimension: wgpu::TextureDimension::D2, 
+            format: context.config.format, 
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, 
+            view_formats: &context.config.view_formats,
+        });
+    }
+}
+
+impl From<&Framebuffer> for wgpu::TextureView {
+
+    fn from(value: &Framebuffer) -> Self {
+        value.texture.create_view(&wgpu::TextureViewDescriptor::default())
+    }
+}
--- src/window.rs
+++ src/window.rs
@@ -3,7 +3,7 @@ use serde::{Serialize, Deserialize};
 use winit::{event_loop::{EventLoop, ControlFlow}, window::{WindowBuilder}, dpi::{PhysicalSize, LogicalPosition}, event::{WindowEvent, MouseScrollDelta}};
 use winit_fullscreen::WindowFullScreen;
 
-use crate::{core::ModuleStack, event};
+use crate::{core::ModuleStack, event, context::{Context}};
 
 #[derive(Serialize, Deserialize)]
 pub struct WindowConfig {
@@ -55,99 +55,99 @@ impl Window {
         Window { native: window, event_loop: event_loop }
     }
 
-    pub fn dispatch_gamepad_event(apps: &mut ModuleStack, event: &gilrs::Event, _control_flow: &mut ControlFlow) -> bool
+    pub fn dispatch_gamepad_event(apps: &mut ModuleStack, event: &gilrs::Event, _control_flow: &mut ControlFlow, context: &Context) -> bool
     {
         match event.event {
             EventType::Connected => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadConnected { id: event.id })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadConnected { id: event.id }, context)
             },
             EventType::Disconnected => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadDisconnected { id: event.id })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadDisconnected { id: event.id }, context)
             },
             EventType::ButtonPressed(button, ..) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Pressed })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Pressed }, context)
             },
             EventType::ButtonReleased(button, ..) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Released })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Released }, context)
             },
             EventType::ButtonRepeated(button, ..) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Repeated })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInput { id: event.id, buttoncode: button, state: event::GamepadButtonState::Repeated }, context)
             },
             EventType::ButtonChanged(button, value, ..) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInputChanged { id: event.id, scancode: button as u32, value: value })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadInputChanged { id: event.id, scancode: button as u32, value: value }, context)
             },
             EventType::AxisChanged(axis, value, ..) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadAxis { id: event.id, axiscode: axis, value: value })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::GamepadAxis { id: event.id, axiscode: axis, value: value }, context)
             },
             _ => {false}
         }
     }
 
-    pub fn dispatch_event(apps: &mut ModuleStack, event: &WindowEvent, control_flow: &mut ControlFlow) -> bool
+    pub fn dispatch_event(apps: &mut ModuleStack, event: &WindowEvent, control_flow: &mut ControlFlow, context: &Context) -> bool
     {
         match event {
             WindowEvent::Resized(size) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::Resized { width: size.width, height: size.height })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::Resized { width: size.width, height: size.height }, context)
             },
             WindowEvent::Moved(pos) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::Moved { x: pos.x, y: pos.y })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::Moved { x: pos.x, y: pos.y }, context)
             },
             WindowEvent::CloseRequested => {
-                let return_value = apps.dispatch_event(event::EventType::Layer, &event::Event::CloseRequested);
+                let return_value = apps.dispatch_event(event::EventType::Layer, &event::Event::CloseRequested, context);
                 control_flow.set_exit();
                 return return_value;
             },
             WindowEvent::Destroyed => {
-                let return_value = apps.dispatch_event(event::EventType::Layer, &event::Event::Destroyed);
+                let return_value = apps.dispatch_event(event::EventType::Layer, &event::Event::Destroyed, context);
                 control_flow.set_exit();
                 return return_value;
             },
             WindowEvent::DroppedFile(path) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::DroppedFile(path.clone()))
+                apps.dispatch_event(event::EventType::Layer, &event::Event::DroppedFile(path.clone()), context)
             },
             WindowEvent::HoveredFile(path) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::HoveredFile(path.clone()))
+                apps.dispatch_event(event::EventType::Layer, &event::Event::HoveredFile(path.clone()), context)
             },
             WindowEvent::HoveredFileCancelled => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::HoveredFileCancelled)
+                apps.dispatch_event(event::EventType::Layer, &event::Event::HoveredFileCancelled, context)
             },
             WindowEvent::ReceivedCharacter(ch) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::ReceivedCharacter(*ch))
+                apps.dispatch_event(event::EventType::Layer, &event::Event::ReceivedCharacter(*ch), context)
             },
             WindowEvent::Focused( focused) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::Focused(*focused))
+                apps.dispatch_event(event::EventType::Layer, &event::Event::Focused(*focused), context)
             },
             WindowEvent::KeyboardInput { input, .. } => {
                 if input.virtual_keycode.is_some() {
-                    apps.dispatch_event(event::EventType::Layer, &event::Event::KeyboardInput { keycode: input.virtual_keycode.unwrap(), state: input.state })
+                    apps.dispatch_event(event::EventType::Layer, &event::Event::KeyboardInput { keycode: input.virtual_keycode.unwrap(), state: input.state }, context)
                 } else {
                     return false;
                 }
             },
             WindowEvent::ModifiersChanged( state ) => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::ModifiersChanged(*state))
+                apps.dispatch_event(event::EventType::Layer, &event::Event::ModifiersChanged(*state), context)
             },
             WindowEvent::CursorMoved { position, .. } => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorMoved { x: position.x, y: position.y })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorMoved { x: position.x, y: position.y }, context)
             },
             WindowEvent::CursorEntered { .. } => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorEntered)
+                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorEntered, context)
             },
             WindowEvent::CursorLeft { .. } => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorLeft)
+                apps.dispatch_event(event::EventType::Layer, &event::Event::CursorLeft, context)
             },
             WindowEvent::MouseWheel { delta, phase, .. } => {
                 match delta {
                     MouseScrollDelta::PixelDelta( d) => {
-                        apps.dispatch_event(event::EventType::Layer, &event::Event::MouseWheel { delta_x: d.x, delta_y: d.y, state: *phase})
+                        apps.dispatch_event(event::EventType::Layer, &event::Event::MouseWheel { delta_x: d.x, delta_y: d.y, state: *phase}, context)
                     },
                     MouseScrollDelta::LineDelta(x, y) => {
-                        apps.dispatch_event(event::EventType::Layer, &event::Event::MouseScroll { delta_x: *x, delta_y: *y, state: *phase})
+                        apps.dispatch_event(event::EventType::Layer, &event::Event::MouseScroll { delta_x: *x, delta_y: *y, state: *phase}, context)
                     }
                 }  
             },
             WindowEvent::MouseInput { state, button, .. } => {
-                apps.dispatch_event(event::EventType::Layer, &event::Event::MouseInput { mousecode: *button, state: *state })
+                apps.dispatch_event(event::EventType::Layer, &event::Event::MouseInput { mousecode: *button, state: *state }, context)
             }
 
             _ => {false}
